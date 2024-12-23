baf6d01b8029786b8cea2de499686ad73cfff63b
Pallavi011-oss <pallavi_soni@pibmindore.ac.in>
2024-12-23 16:20:44 +0530

    Update streamlit_regression.py

--- streamlit_regression.py
+++ streamlit_regression.py
@@ -9,13 +9,13 @@ import pickle
 model = tf.keras.models.load_model('regression_model.h5')
 
 # load the encoders and scaler 
-with open('label_encoder_gender.pkl', 'rb') as file:
+with open('label_encoder_gender_regression.pkl', 'rb') as file:
   label_encoder_gender = pickle.load(file)
 
-with open('onehot_encoder_geo.pkl','rb') as file:
+with open('onehot_encoder_geo_regression.pkl','rb') as file:
   onehot_encoder_geo = pickle.load(file)
 
-with open('scaler.pkl', 'rb') as file:
+with open('scaler_regression.pkl', 'rb') as file:
    scaler = pickle.load(file)
 
 ## Sreamlit app
